2a65d17215abc4f543a159547670df3161ce0e6a
Marcio-Brust <brustmw21@gmail.com>
2023-03-02 01:56:07 -0300

    ajustesFinais

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -2,28 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className=" box-border  flex items-center justify-between bg-black p-5 font-semibold text-cyan-50 ">
+    <header className=" box-border  flex items-center justify-center bg-black p-5 font-semibold text-cyan-50 ">
       <h1 className=" text-h1 text-orange-300">POKEMON</h1>
-      <button className="btn-menu px-10 pt-5  pb-5 hover:text-orange-300 sm:px-10 md:pl-10  ">
-        MENU
-      </button>
-      <ul className="menu flex">
-        <li className=" cursor-pointer px-10  pt-5  pb-5  hover:text-orange-300 sm:px-10  md:pl-10">
-          Todos
-        </li>
-        <li className="  cursor-pointer px-10  pt-5  pb-5  hover:text-orange-300 sm:px-10  md:pl-10">
-          Tipos
-        </li>
-        <li className=" cursor-pointer px-10  pt-5  pb-5  hover:text-orange-300 sm:px-10  md:pl-10 ">
-          Subtipos
-        </li>
-        <li className="  cursor-pointer px-10   pt-5 pb-5  hover:text-orange-300 sm:px-10 md:pl-10 ">
-          Supertipos
-        </li>
-        <li className="  cursor-pointer px-10  pt-5 pb-5  hover:text-orange-300 sm:px-10  md:pl-10 ">
-          Raros
-        </li>
-      </ul>
     </header>
   );
 };
--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -4,11 +4,16 @@ import { useFetch } from "../Hooks/useFetch";
 const Home = () => {
   const { data } = useFetch("https://api.pokemontcg.io/v2/cards");
 
+  function handleModal() {
+    console.log("oi");
+  }
+
   return (
     <div className="box-border grid grid-cols-1  gap-8   bg-black  p-10 sm:grid-cols-3 md:grid-cols-4">
       {data?.map((item, index) => (
-        <div className=" grid justify-center" key={index}>
+        <div onClick={handleModal} className=" grid justify-center" key={index}>
           <img
+            id="img"
             className=" cursor-pointer"
             src={item.images.small}
             alt="imagem"
