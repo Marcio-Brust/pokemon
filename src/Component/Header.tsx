f779f8970cfa85980b598da61458338b7d0692b6
Marcio-Brust <brust21mw@gmail.com>
2023-05-15 21:47:51 -0300

    barra de pesquisa

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/img/pokemon-4096.png";
 
 const Header = () => {
   return (
-    <header className=" box-border flex items-center  justify-evenly p-5">
+    <header className=" box-border flex items-center  justify-center">
       <img
         style={{ width: "220px", display: "block" }}
         src={logo}
--- src/Component/Home.tsx
+++ src/Component/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useFetch } from "../Hooks/useFetch";
 import { Loading } from "../styled/Loading";
+import gif from "../assets/img/pokeball-throwing.gif";
 
 const Home = () => {
   const api = "https://api.pokemontcg.io/v2/";
@@ -11,7 +12,19 @@ const Home = () => {
     (item, index) => dataName.indexOf(item) === index
   );
 
-  const [name, setName] = useState("Ampharos");
+  const [name, setName] = useState("Lugia");
+
+  function searchName() {
+    const namePokemon = document.querySelector("input");
+
+    if (namePokemon) {
+      const nomePokemon = dataName?.find((item) => item === namePokemon.value);
+
+      if (nomePokemon) {
+        setName(nomePokemon);
+      }
+    }
+  }
 
   function handleName({ target }) {
     const p = document.querySelectorAll("p");
@@ -23,11 +36,31 @@ const Home = () => {
     target.classList.toggle("active");
     setName(target.innerText);
   }
-
+  if (error) return <div>oi</div>;
   if (loading) return <Loading style={{ margin: "0 auto" }}></Loading>;
   return (
     <>
-      <section className="bodyFull box-border flex justify-evenly">
+      <div className="searchDiv">
+        {" "}
+        <label
+          htmlFor="search"
+          style={{
+            cursor: "pointer",
+            fontSize: "1.1rem",
+            fontWeight: "bold",
+            fontFamily: "monospace",
+          }}
+        >
+          Buscar Pokemon
+        </label>
+        <div className="flex">
+          <input type="text" className="search" name="search" id="search" />
+          <button className="btn" onClick={searchName}>
+            <img src={gif} alt="imagem" />
+          </button>
+        </div>
+      </div>
+      <section className="bodyFull  box-border flex justify-center">
         <div
           className="name-pokemon"
           style={{
@@ -56,11 +89,21 @@ const Home = () => {
           {data
             ?.filter((item) => item.name === `${name}`)
             .map((item) => (
-              <img
-                className="imgPokemon"
-                src={item.images.small}
-                alt="imagem"
-              />
+              <div className=" flex">
+                <img
+                  className="imgPokemon"
+                  src={item.images.small}
+                  alt="imagem"
+                />
+                <div className="ml-5 grid">
+                  <span>Tipo: {item.types.toString()}</span>
+                  <span>Level: {item.level}</span>
+                  <span>Hp: {item.hp}</span>
+                  <span>Raridade: {item.rarity}</span>
+                  <span>Subtipos: {item.subtypes}</span>
+                  <span>Supertipos: {item.supertype}</span>
+                </div>
+              </div>
             ))
             .reverse()
             .pop()}
